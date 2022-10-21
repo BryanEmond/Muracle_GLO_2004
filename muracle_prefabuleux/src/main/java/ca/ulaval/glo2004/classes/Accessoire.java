3af070a238abfe0056d449259a5096bd9d4f60d0
Roxmo56633 <dionneroxane@gmail.com>
2022-10-20 22:01:47 -0400

    ajout accessoire2

--- muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/classes/Accessoire.java
+++ muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/classes/Accessoire.java
@@ -10,7 +10,7 @@ int positionY;
 
 ArrayList<ArrayList<Integer>> positions = new Arraylist<ArrayList<Integer>>();
 ArrayList<Integer> point = new ArrayList<Integer>();
-
+ArrayList<Integer> point2 = new ArrayList<Integer>();
     private Accessoire (int hauteur, int largeur, int positionX, int positionY){
         this.hauteur = hauteur;
         this.largeur = largeur;
@@ -18,14 +18,21 @@ ArrayList<Integer> point = new ArrayList<Integer>();
         this.positionY = positionY;
     }
 
-public boolean validerContrainte() {return true;}
+
     public ArrayList getZone() {
 
         //option 1: positionX et Y = point au centre
         point.add(positionX - largeur/2);
         point.add(positionY + hauteur/2);
+        point2.add(positionX +largeur/2);
+        point2.add(positionY - hauteur/2);
+
         positions.add(point);
+        positions.add(point2);
         return positions;}
 
-
+    public boolean validerContrainte() {
+        //a précisé pour chaque type d'accessoire
+return true;
+    }
 }
