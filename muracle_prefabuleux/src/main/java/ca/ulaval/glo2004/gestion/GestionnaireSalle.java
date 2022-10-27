52199dde5ade5e8b868116c31d4223624abaeafd
Alexandre Dicaire <alexandre.dicaire.1@ulaval.ca>
2022-10-26 22:46:57 -0400

    Création GestionnaireSalle

---
+++ muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/gestion/GestionnaireSalle.java
@@ -0,0 +1,32 @@
+package ca.ulaval.glo2004.gestion;
+
+public class GestionnaireSalle {
+
+
+    public void creerSalle()
+    {
+
+    }
+
+    public void enregistrerSalle()
+    {
+
+    }
+
+    public void chargerSalle(String cheminFichier)
+    {
+
+    }
+
+    public void exporerSVG(String cheminDossier)
+    {
+
+    }
+
+    public void quitter()
+    {
+        //TODO Ne pas oublier d'implémenter la vérification de sauvegarde un jour
+        System.exit(0);
+    }
+
+}
