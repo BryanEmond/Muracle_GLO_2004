7a9df675d9f62332e340d76bcd5a43456d61b3cd
BryanEmond <thunderghost22@outlook.com>
2022-11-25 18:10:38 -0500

    plan separateur

--- muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/classes/Salle.java
+++ muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/classes/Salle.java
@@ -133,8 +133,9 @@ public class Salle implements Serializable {
                     var.AjouterSeparateur(new Separateur(point.mY,point.mX,distanceBord,var,new Polygone(Color.BLACK,points)));
                     }
                 };
+            return direction;
             }
-        return direction;
+        return null;
     }
 
     private Polygone getPolygoneMur(Cote var, PointImperial point) {
